1ef265aa452a4dda6db35a0d95efff17dc50519f
cryptoscuttlebutt <35452123+cryptoscuttlebutt@users.noreply.github.com>
2018-07-05 13:54:23 +0200

    Update template.class.php

--- includes/template.class.php
+++ includes/template.class.php
@@ -91,7 +91,7 @@ class Template
                                             $this->template );
         }
 
-        $this->template = preg_replace( "/" .$this->leftDelimiterC ."(.*)" .$this->rightDelimiterC ."/isUe",
+        $this->template = preg_replace( "/" .$this->leftDelimiterC ."(.*)" .$this->rightDelimiterC ."/isU",
                                         "", $this->template );
     }
 
